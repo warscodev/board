2c2be5823264972d36eb243e2b9592701b12e7ac
admin <admin@DESKTOP-5KKA51A>
2019-11-25 17:11:17 +0900

    아파치 Common Fileupload 라이브러리 추가, MultipartResolver 구현 및 빈 등록, 어플리케이션에서 첨부파일 관련된 자동구성 제외

--- src/main/java/board/BoardApplication.java
+++ src/main/java/board/BoardApplication.java
@@ -2,8 +2,10 @@ package board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
 
-@SpringBootApplication
+// multipartResolver를 따로 등록했기 때문에 첨부파일과 관련된 자동구성을 사용하지 않도록 변경함.
+@SpringBootApplication(exclude = {MultipartAutoConfiguration.class})
 public class BoardApplication {
 
 	public static void main(String[] args) {
--- src/main/java/board/configuration/WebMvcConfiguration.java
+++ src/main/java/board/configuration/WebMvcConfiguration.java
@@ -1,6 +1,8 @@
 package board.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -8,6 +10,18 @@ import board.interceptor.LoggerInterceptor;
 
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer{
+	
+	@Bean
+	public CommonsMultipartResolver multipartResolver() {
+		CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver();
+		
+		//파일 인코딩 설정
+		commonsMultipartResolver.setDefaultEncoding("UTF-8");
+		//업로드 되는 파일 크기 제한. 바이트(byte)단위로 설정 가능. 여기서는 5mb로 설정.
+		commonsMultipartResolver.setMaxUploadSizePerFile(5 * 1024 * 1024);
+		
+		return commonsMultipartResolver;
+	}
 	
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
