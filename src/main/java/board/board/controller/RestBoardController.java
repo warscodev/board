66cf2f164697c08937c5350934f924bd2b6d7033
warsco <warsco@DESKTOP-TMOMNA2>
2020-01-21 14:50:10 +0900

    RestBoardController 완성

--- src/main/java/board/board/controller/RestBoardController.java
+++ src/main/java/board/board/controller/RestBoardController.java
@@ -1,16 +1,24 @@
 package board.board.controller;
 
+import java.io.File;
+import java.net.URLEncoder;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
 
 import board.board.dto.BoardDto;
+import board.board.dto.BoardFileDto;
 import board.board.service.BoardService;
 
 @Controller
@@ -56,21 +64,30 @@ public class RestBoardController {
 		return "redirect:board";
 	}
 	
+	@RequestMapping(value="/board/{boardIdx}", method=RequestMethod.DELETE)
+	public String deleteBoard(@PathVariable("boardIdx") int boardIdx) throws Exception{
+		boardService.deleteBoard(boardIdx);
+		return "redirect:/board";
+	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-
+	@RequestMapping(value="/board/file", method=RequestMethod.GET)
+	public void downloadBoardFile(@RequestParam int idx, @RequestParam int boardIdx, HttpServletResponse response) throws Exception{
+		BoardFileDto boardFile = boardService.selectBoardFileInformation(idx, boardIdx);
+		
+		if(ObjectUtils.isEmpty(boardFile) == false) {
+			String fileName = boardFile.getOriginalFileName();
+			
+			byte[] files = FileUtils.readFileToByteArray(new File(boardFile.getStoredFilePath()));
+			
+			response.setContentType("applicatoin/octet-stream");
+			response.setContentLength(files.length);
+			response.setHeader("Content-Disposition", "attachment; fileName=\"" +
+					URLEncoder.encode(fileName, "UTF-8")+"\";");
+			
+			response.getOutputStream().write(files);
+			response.getOutputStream().flush();
+			response.getOutputStream().close();
+			
+		}
+	}
 }
